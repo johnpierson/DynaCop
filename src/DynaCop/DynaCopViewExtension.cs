6abb167c5c7eccd9a72884ae3a376d9aea7fb73e
john pierson <sixtysecondrevit@gmail.com>
2022-01-11 16:43:01 -0700

    Going to need to investigate more

--- src/DynaCop/DynaCopViewExtension.cs
+++ src/DynaCop/DynaCopViewExtension.cs
@@ -3,38 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using Dynamo.Extensions;
 using Dynamo.Linting;
+using Dynamo.ViewModels;
 using Dynamo.Wpf.Extensions;
 
 namespace DynaCopViewExtension
 {
-    public class DynaCopViewExtension : ViewExtensionBase
+    public class DynaCopViewExtension : IViewExtension
     {
         private const string ExtensionName = "DynaCop View Extension";
         private const string _ExtensionUniqueId = "EF61CF18-ABB1-4FF9-8F42-9870FC08DF83";
         public override string UniqueId => _ExtensionUniqueId;
         public override string Name => ExtensionName;
 
-        private LinterManager linterManager;
+        private MenuItem dynaCopMenuItem;
         private ViewLoadedParams viewLoadedParamsReference;
-        private MenuItem linterMenuItem;
-        private LinterViewModel linterViewModel;
-        private LinterView linterView;
 
+        public override void Ready(ReadyParams sp)
+        {
+            base.Ready(sp);
+
+            this.AddLinterRule(new NodesCantBeNamedFooRule());
+            this.AddLinterRule(new InputNodesNotAllowedRule());
+            this.AddLinterRule(new GraphNeedsOutputNodesRule());
+            this.AddLinterRule(new WarningSeverityRule());
+            this.AddLinterRule(new DuplicatedNamesRule());
+        }
 
         public override void Startup(ViewStartupParams viewStartupParams)
         {
             // Do nothing for now
         }
 
-
-
-        public override void Dispose()
+        public void Loaded(ViewLoadedParams viewLoadedParams)
         {
-            throw new NotImplementedException();
+            this.viewLoadedParamsReference = viewLoadedParams;
+            this.dynaCopMenuItem = new MenuItem { Header = "TestLinter", IsCheckable = true };
+            this.dynaCopMenuItem.Checked += MenuItemCheckHandler;
+            this.viewLoadedParamsReference.AddExtensionMenuItem(this.dynaCopMenuItem);
+        }
+        private void MenuItemCheckHandler(object sender, RoutedEventArgs e)
+        {
+            var viewModel = this.viewLoadedParamsReference.DynamoWindow.DataContext as DynamoViewModel;
+            var linterManager = viewModel.Model.LinterManager;
+
+            foreach (var linter in linterManager.AvailableLinters)
+            {
+                if (linter.Id == this.UniqueId)
+                    linterManager.SetActiveLinter(linter);
+
+            }
         }
 
-        
+        public override void Dispose() { }
+        public override void Shutdown() { }
+
+
     }
 }
